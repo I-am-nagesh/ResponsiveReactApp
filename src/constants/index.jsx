3e2cf4ecd009f8a4a505ea68cf95c05c97347d64
Nagesh Pandey <i.am.nageshpandey@gmail.com>
2024-08-26 16:18:55 +0530

    footer sction done

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -66,3 +66,27 @@ export const checklistItems = [
         "Track the performance of your VR apps and gain insights into user behavior.",
     },
 ];
+
+export const resourcesLinks = [
+    { href: "#", text: "Getting Started" },
+    { href: "#", text: "Documentation" },
+    { href: "#", text: "Tutorials" },
+    { href: "#", text: "API Reference" },
+    { href: "#", text: "Community Forums" },
+  ];
+  
+  export const platformLinks = [
+    { href: "#", text: "Features" },
+    { href: "#", text: "Supported Devices" },
+    { href: "#", text: "System Requirements" },
+    { href: "#", text: "Downloads" },
+    { href: "#", text: "Release Notes" },
+  ];
+  
+  export const communityLinks = [
+    { href: "#", text: "Events" },
+    { href: "#", text: "Meetups" },
+    { href: "#", text: "Conferences" },
+    { href: "#", text: "Hackathons" },
+    { href: "#", text: "Jobs" },
+  ];
