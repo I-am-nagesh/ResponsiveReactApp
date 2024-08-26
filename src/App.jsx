2ee3d502af2701af30ae0728c053938284ffda3d
Nagesh Pandey <i.am.nageshpandey@gmail.com>
2024-08-26 17:58:10 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import HeroSection from "./components/HeroSection";
 import FeatureSection from "./components/FeatureSection";
 import Workflow from "./components/Workflow";
 import Footer from "./components/Footer";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
         <HeroSection />
         <FeatureSection />
         <Workflow />
+        <Pricing />
+        <Testimonials />
         <Footer />
       </div>
     </>
